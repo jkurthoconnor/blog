1738547d883b7c3f4c77f99f37e13f7e24ee892f
jkurthoconnor <jkurthoconnor@gmail.com>
2019-03-20 16:35:41 -0600

    Update modular approach

--- Gemfile
+++ Gemfile
@@ -1,6 +1,10 @@
 source 'https://rubygems.org'
 
 ruby '2.5.0'
+
 gem 'sinatra'
 gem 'thin'
-gem 'pry'
+
+group :development do
+  gem 'pry'
+end
--- blog.rb
+++ blog.rb
@@ -1,13 +1,23 @@
 require 'sinatra/base'
-require 'pry'
 
 class Blog < Sinatra::Base
-  get '/' do
-    "hello world!"
+  configure do
+    set :root, File.dirname(__FILE__)
   end
 
-#
+  configure :development do
+    require 'pry'
+    set :logging, true
+  end
+
+
+  get '/' do
+    settings.root
+    settings.app_file
+  end
+
 # run server when file is invoked directly;
+# allows rackup, tests, etc to run file indirectly
   if __FILE__ == $0
     run!
   end
