229013a5494d6ad96e8781845e76b5f50fb46d86
jkurthoconnor <jkurthoconnor@gmail.com>
2019-03-21 02:00:59 -0600

    Add mock views and reloading gem

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 ruby '2.5.0'
 
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'thin'
 
 group :development do
--- blog.rb
+++ blog.rb
@@ -1,19 +1,43 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
 
 class Blog < Sinatra::Base
+
   configure do
     set :root, File.dirname(__FILE__)
   end
 
   configure :development do
     require 'pry'
+    register Sinatra::Reloader
     set :logging, true
+    set :show_exceptions, :after_handler
   end
 
-
   get '/' do
-    settings.root
-    settings.app_file
+    @title = "Kurth O'Connor"
+    @test_string = "Project root is `#{self.class.root}`"
+    erb :landing
+  end
+
+  get '/blog' do
+    @title = "Blog"
+    @test_string = "This is a blog title"
+    erb :blog
+  end
+
+  get '/:value/:other_val' do
+    @title = "Sample params"
+    @test_string = "#{params[:value]} : #{params[:other_val]}\n\n"
+    erb :blog
+  end
+
+  not_found do
+    redirect '/'
+  end
+
+  error 500 do
+    puts "How could you break this?"
   end
 
 # run server when file is invoked directly;
